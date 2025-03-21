162ce86ce5d3e9f935e6ed65c43906e7db3551a9
Riam Martinelli <107009834+richboyyy@users.noreply.github.com>
2025-03-21 09:55:49 -0300

    Add files via upload

---
+++ CriptografiaAES.java
@@ -0,0 +1,67 @@
+package aula_03;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class CriptografiaAES {
+	public static void main(String[] args) {
+		// Declarações de Variáveis
+		BufferedReader leitor = new BufferedReader(
+								new InputStreamReader(
+								System.in));
+		
+		String texto = "";
+		String chave = "";
+		String criptograma = "";
+		
+		// Processamento
+		try {
+			System.out.print("Digite o texto: ");
+			texto = leitor.readLine();
+			
+			System.out.print("Digite a chave: ");
+			chave = leitor.readLine();
+			
+			criptograma = encriptar(texto, chave);
+			
+			System.out.println(criptograma);
+			
+			System.out.println(decriptar(criptograma, chave));
+			
+		} catch(Exception erro) {
+			System.out.println("FODEU GRANDE !" + erro);
+		
+		}
+	}
+	
+	public static String encriptar(String texto, String chave)
+														throws Exception {
+		String criptograma = "";
+		
+		Cipher cifra = Cipher.getInstance("AES/CBC/PKCS5Padding");
+		cifra.init(Cipher.ENCRYPT_MODE, 
+				new SecretKeySpec(chave.getBytes("UTF-8"), "AES"),
+				new IvParameterSpec("JuniorBonitao123".getBytes()));
+		
+		byte[] vetor = cifra.doFinal(texto.getBytes("UTF-8"));
+		return Base64 .getEncoder().encodeToString(vetor);
+	}
+	public static String decriptar(String texto, String chave)
+			throws Exception {
+		String criptograma = "";
+
+		Cipher cifra = Cipher.getInstance("AES/CBC/PKCS5Padding");
+		cifra.init(Cipher.ENCRYPT_MODE, 
+				new SecretKeySpec(chave.getBytes("UTF-8"), "AES"),
+				new IvParameterSpec("JuniorBonitao123".getBytes()));
+
+		byte[] vetor = cifra.doFinal(
+									Base64.getDecoder().decode(criptograma));
+		return new String(vetor, "UTF-8");
+	}
+}
